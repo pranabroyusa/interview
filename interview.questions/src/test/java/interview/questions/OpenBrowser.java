8de4a36c9ff52c95b2cb3e9f73d811d805afd3e7
Pranab Roy <Pranab Roy@Pranab.home>
2019-04-12 15:12:44 -0700

    1st commit

---
+++ interview.questions/src/test/java/interview/questions/OpenBrowser.java
@@ -0,0 +1,24 @@
+package interview.questions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	static WebDriver driver;
+	public static void main(String[] args)
+	{
+		selectbrowser();
+	}
+	
+	
+	
+	public static void selectbrowser()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Pranab Roy\\Desktop\\Drivers\\chromedriver.exe");
+	    driver=new ChromeDriver();
+	    driver.get("https://www.macys.com/");
+	}
+
+}
